e71f5bbaa611a9f30abedcd4dfa5ff58fde774fa
kimmywingz <fishpeopleapplications@gmail.com>
2023-07-03 12:03:13 -0600

    initial commit
    logic is started, 1 of 5 sets of flags are imported, background and extensions complete

---
+++ GuessTheStateFlag/GuessTheStateFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheStateFlagApp.swift
+//  GuessTheStateFlag
+//
+//  Created by KBrewer on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheStateFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
