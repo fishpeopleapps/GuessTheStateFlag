7886ec75304d2850af5a1ae89c0f0d472195f4c3
kimmywingz <fishpeopleapplications@gmail.com>
2023-07-03 20:14:30 -0600

    end of day 1
    almost all of the flags are in, logic is there for keeping score and moving on

--- GuessTheStateFlag/ContentView.swift
+++ GuessTheStateFlag/ContentView.swift
@@ -9,6 +9,8 @@
 // TODO: Add haptics if the user gets it wrong
 // TODO: ooh add a bonus point if you can guess the capital
 // TODO: make an app icon
+// TODO: make the image sizes dynamic
+// TODO: allow users to reset their score
 
 import SwiftUI
 
@@ -16,7 +18,12 @@ struct ContentView: View {
     /// Detects if the user is using light or dark mode
     @Environment(\.colorScheme) var colorScheme
     /// We will be displaying three flags, this creates a correct answer within the three displayed
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingAlert = false
+    @State private var alertTitle = "Wrong"
+    @AppStorage("score") var score: Int = 0
+
     var body: some View {
         ZStack {
             AngularGradient(
@@ -27,32 +34,57 @@ struct ContentView: View {
                 VStack {
                     Text("Guess the Flag of")
                         .titleTextStyle()
-                    Text(states[correctAnswer])
+                    Text(stateList[correctAnswer].stateName)
                         .headlineTextStyle()
                 }
-                VStack(spacing: 20) {
+                VStack(spacing: 10) {
                     ForEach(0..<3) { num in
                         Button {
-                            // flagged was tapped
+                            checkAnswer(num)
+                            print("Displaying \(stateList[num].stateName)")
                         } label: {
-                            Image(states[num])
+                            Image(stateList[num].stateName)
                                 .cornerRadius(CGFloat(40), corners: [.topRight, .bottomLeft])
                                 .shadow(color: .white, radius: 5)
+
                         }
                     }
+                    Text("Current Score: \(score)")
+                        .headlineTextStyle()
+                    Button("Reset Score") {
+                        score = 0
+                    }.tint(.secondary)
                 }
                 .padding()
             }
-           // .background(.ultraThinMaterial) // I don't like this
-            .cornerRadius(15)
+
         }
         .ignoresSafeArea()
+        .alert(alertTitle, isPresented: $showingAlert) {
+            Button("Continue", action: nextQuestion)
+        } message: {
+            Text("Your score is \(score)")
+        }
+    }
+    
+    func checkAnswer(_ num: Int) {
+        if num == correctAnswer {
+            score += 1
+            nextQuestion()
+        } else {
+            showingAlert = true
+        }
+    }
+    
+    func nextQuestion() {
+        stateList.shuffle()
+        correctAnswer = Int.random(in: 0..<2)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .preferredColorScheme(.light)
+            .preferredColorScheme(.dark)
     }
 }
